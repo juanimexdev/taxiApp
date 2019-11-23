ef5bb1d934e4e2e86f3a1951b29781f91cb99a07
juanita <juaniyta_3w0hotmail.com>
2019-11-22 18:59:26 -0600

    requestrip view

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import Splash from '../containers/Splash';
-import Login from '../containers/Login';
-import SignUp from '../containers/SignUp';
-import Home from '../containers/Home';
-import TaxiSelect from '../containers/TaxiSelect';
-import Trip from '../containers/Trip';
+import React from "react";
+import { Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Splash from "../containers/Splash";
+import Login from "../containers/Login";
+import SignUp from "../containers/SignUp";
+import Home from "../containers/Home";
+import TaxiSelect from "../containers/TaxiSelect";
+import Trip from "../containers/Trip";
+import RequestTrip from "../containers/RequestTrip";
 
 const history = createBrowserHistory();
 const App = () => (
@@ -18,6 +19,7 @@ const App = () => (
       <Route exact path='/home' component={Home} />
       <Route exact path='/taxiselect' component={TaxiSelect} />
       <Route exact path='/trip' component={Trip} />
+      <Route exact path='/request-trip' component={RequestTrip} />
     </Switch>
   </Router>
 );
